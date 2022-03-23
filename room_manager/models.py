b94ab293434a41d34171425f91a5993f975d16a7
Justyna Łuczak <justyna.macniak@gmail.com>
2022-03-23 15:51:39 +0100

    model for reservation to check availability of conference room on date

--- room_manager/models.py
+++ room_manager/models.py
@@ -7,3 +7,12 @@ class ConferenceRoom(models.Model):
     name = models.CharField(max_length=255, unique=True)
     capacity = models.IntegerField(validators=[MinValueValidator(10),])
     projector = models.BooleanField(default=False)
+
+
+class ReserveRoom(models.Model):
+    room_id = models.ForeignKey(ConferenceRoom, on_delete=models.CASCADE)
+    date = models.DateField()
+    comment = models.TextField(null=True)
+
+    class Meta:
+        unique_together = ('room_id', 'date',)
