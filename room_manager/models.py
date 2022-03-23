c5134e2f26ed2dbff39835b0068f3d629486c0ee
Justyna Łuczak <justyna.macniak@gmail.com>
2022-03-23 14:20:50 +0100

    capacity validator with minimum value when add conference room

--- room_manager/models.py
+++ room_manager/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
 # Create your models here.
 
 
 class ConferenceRoom(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    capacity = models.IntegerField()
+    capacity = models.IntegerField(validators=[MinValueValidator(10),])
     projector = models.BooleanField(default=False)
--- room_manager/views.py
+++ room_manager/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import CreateView
 from django.views.generic.list import ListView
 
 # Create your views here.
@@ -15,3 +17,13 @@ class RoomListView(ListView):
     paginate_by = 12
     context_object_name = 'list_of_all_rooms_in_database'
     template_name = 'room_manager/conferenceroom_list.html'
+
+
+class AddRoomView(CreateView):
+
+    model = ConferenceRoom
+    fields = ['name', 'capacity', 'projector']
+    context_object_name = 'add_new_room_to_database'
+
+    def get_success_url(self):
+        return reverse('room_list',)
